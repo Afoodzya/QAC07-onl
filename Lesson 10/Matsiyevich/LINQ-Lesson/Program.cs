ea83e8ee4c26c485adaf0b22ba81e0a6704dd07e
Pavel <pawellm96@gmail.com>
2025-10-06 18:26:56 +0300

    Lesson 10 - LINQ

---
+++ Lesson 10/Matsiyevich/LINQ-Lesson/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LINQ_Lesson
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
